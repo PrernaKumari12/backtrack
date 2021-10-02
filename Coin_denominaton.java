069567be830098673ca4eecd661823ab01af158d
PrernaKumari12 <91779020+PrernaKumari12@users.noreply.github.com>
2021-10-02 14:15:45 +0530

    Add files via upload

---
+++ Coin_denominaton.java
@@ -0,0 +1,23 @@
+package Backtracking;
+
+public class Coin_denominaton {
+	public static void main(String[] args) {
+		int[]arr= {2,3,5,6};
+		coin(10, arr, "");
+		
+	}
+	public static void coin(int amount,int[] arr,String ans) {
+		if(amount==0) {
+			System.out.println(ans);
+			return;
+		}
+		if(amount<0) {
+			return;
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+		coin(amount-arr[i], arr,ans+arr[i]);
+		
+	}}
+
+}
